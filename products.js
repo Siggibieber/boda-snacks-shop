ba4fdfb8f43ea08f9d98a4c5a6ab9f308d30139a
Siggibieber <bieber1987@gmx.net>
2026-07-14 17:22:49 +0200

    Create products.js

---
+++ products.js
@@ -0,0 +1,35 @@
+const products = [
+
+{
+    id:1,
+    name:"Pringles Paprika",
+    category:"Chips",
+    price:2.99,
+    image:"img/products/pringles.png"
+},
+
+{
+    id:2,
+    name:"Haribo Goldbären",
+    category:"Süßigkeiten",
+    price:1.79,
+    image:"img/products/haribo.png"
+},
+
+{
+    id:3,
+    name:"Coca-Cola 0,5 L",
+    category:"Getränke",
+    price:2.49,
+    image:"img/products/coke.png"
+},
+
+{
+    id:4,
+    name:"Oreo Original",
+    category:"Kekse",
+    price:2.29,
+    image:"img/products/oreo.png"
+}
+
+];
